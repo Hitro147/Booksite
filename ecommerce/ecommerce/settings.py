72618b3dd4b5092dc25235fd1ef758943a60080a
Shrikanth Kolluru <rohit147kolluru@gmail.com>
2020-05-04 16:04:00 +0530

    added files for deployment

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'e8w-ur8s&wu0gq!2s=*jly*+9o!^3c3bpz5k6$*4#@m2m4j6te'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bo-k.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
